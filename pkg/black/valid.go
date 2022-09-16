74bd610a5888f972161ec4a97b9b6c5e80b29887
Eugene <Jiztastamablastamarang@gmail.com>
2022-09-16 07:47:43 +0300

    fix valid

--- pkg/black/valid.go
+++ pkg/black/valid.go
@@ -1,8 +1,4 @@
-// DISCLAIMER:
-// all code below is full of black magic
-// and was done a clumsy way
-// btw it works well and gives more flexibility
-// in returning validation errors
+// DISCLAIMER: TL;DR
 
 package black
 
@@ -16,59 +12,65 @@ import (
 
 const defaultKey = "regex"
 
-var ErrUnexpected = errors.New("unexpected error has occurred")
+var (
+	ErrInvalidSrc = errors.New("input value must be struct")
+	ErrUnexpected = errors.New("unexpected error has occurred")
+)
 
 // ValidateStruct validates struct fields
 // according to given regex tag
 func ValidateStruct(src any) (err error) {
-	valOf := reflect.Indirect(reflect.ValueOf(src))
-
-	var structName string
-	if structName == "" {
-		structName = strings.ToLower(valOf.Type().Name())
-	}
-
-	defer func() { // just in case
+	// not sure if we need this,
+	// just in case something go wrong
+	defer func() {
 		if r := recover(); r != nil {
 			err = ErrUnexpected
 		}
 	}()
 
-	for i := 0; i < valOf.NumField(); i++ {
-		field := valOf.Field(i)
-		// check re tag if any is present
-		tags := valOf.Type().Field(i).Tag
-		if tag, ok := getTag(tags, defaultKey); ok {
-			fieldVal := fmt.Sprintf("%v", field.Interface())
+	srcValue := reflect.Indirect(reflect.ValueOf(src))
 
-			if !regexp.MustCompile(tag).MatchString(fieldVal) {
-				fieldName := strings.ToLower(valOf.Type().Field(i).Name)
+	// only structs allowed
+	var structName string
+	if srcType := srcValue.Kind(); srcType != reflect.Struct {
+		return fmt.Errorf("input value must be struct, got: %v", srcType)
+	}
+
+	// name of the top struct (we will scan all nested structs recursively)
+	if structName == "" {
+		structName = strings.ToLower(srcValue.Type().Name())
+	}
+
+	for i := 0; i < srcValue.NumField(); i++ {
+
+		fieldValue := srcValue.Field(i)
+		fieldType := srcValue.Type().Field(i)
+
+		// check regex <key> if any is exists and retrieve its <value> => match[2]
+		tagAll := fmt.Sprintf("%v", srcValue.Type().Field(i).Tag)
+		tagValue := fmt.Sprintf(`(?s)(?i)\s*(?P<key>%s):\"(?P<value>[^\"]+)\"`, defaultKey)
+
+		if match := regexp.MustCompile(tagValue).FindStringSubmatch(tagAll); match != nil {
+
+			fieldToCheck := fmt.Sprintf("%v", fieldValue.Interface())
+			pattern := match[2]
+
+			// validate field value using given regex pattern
+			if !regexp.MustCompile(pattern).MatchString(fieldToCheck) {
+				fieldName := strings.ToLower(fieldType.Name)
+
 				return fmt.Errorf("%s has to have valid %s", structName, fieldName)
 			}
 		}
-		// recursive call for nested structs
-		if field.Type().Kind() == reflect.Struct {
-			if err := ValidateStruct(field.Interface()); err != nil {
-				return err
-			}
+
+		// recursive call
+		if fieldValue.Type().Kind() != reflect.Struct {
+			continue
+		}
+		if err := ValidateStruct(fieldValue.Interface()); err != nil {
+			return err
 		}
 	}
 
 	return err
-}
-
-// getTag help to improve reflect StructField Lookup() method
-// that did not meet our expectations
-func getTag(tag reflect.StructTag, key string) (string, bool) {
-	str := fmt.Sprintf("%v", tag)
-	pattern := fmt.Sprintf(`(?s)(?i)\s*(?P<key>%s):\"(?P<value>[^\"]+)\"`, defaultKey)
-
-	re := regexp.MustCompile(pattern)
-	if !re.MatchString(str) {
-		return "", false
-	}
-
-	match := re.FindStringSubmatch(str)
-
-	return match[2], true
 }
