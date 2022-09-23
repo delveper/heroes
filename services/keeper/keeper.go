8aab5378e822b076dfa0f4a06b7abade1a9bcc85
Eugene <Jiztastamablastamarang@gmail.com>
2022-09-23 20:27:40 +0300

    init

--- services/keeper/keeper.go
+++ services/keeper/keeper.go
@@ -19,10 +19,6 @@ type UserKeeper interface {
 
 type Keeper struct{ *sql.DB }
 
-func NewKeeper(driver *sql.DB) *Keeper {
-	return &Keeper{driver}
-}
-
-func (kpr *Keeper) Add(User) error {
-	return nil
+func NewKeeper(db *sql.DB) *Keeper {
+	return &Keeper{db}
 }
